84b32a1f1c3c220a0047fecb5062d17c23a339a1
Peter Delevoryas <peterdelevoryas@gmail.com>
2017-04-18 21:45:42 -0600

    Added MySQL setup queries

---
+++ eat-net/src/main/sql/create_tables.sql
@@ -0,0 +1,28 @@
+create table eatnet.Restaurants (
+    id int auto_increment not null,
+    name varchar(32) not null,
+    primary key (id)
+);
+
+create table eatnet.BusinessUsers (
+    id int auto_increment not null,
+    username varchar(32) not null unique,
+    password varchar(32) not null,
+    name varchar(64) not null,
+    restaurant int not null,
+    foreign key (restaurant)
+        references Restaurants(id)
+        on delete cascade,
+    primary key (id)
+);
+
+create table eatnet.Items (
+    id int auto_increment not null,
+    name varchar(32) not null,
+    price float not null,
+    restaurant int not null,
+    foreign key (restaurant)
+        references Restaurants(id)
+        on delete cascade,
+    primary key (id)
+);
