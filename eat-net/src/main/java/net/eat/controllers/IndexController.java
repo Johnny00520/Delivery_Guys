279b723e2f9d19ef6254ffbb9ac38153a294dae5
Peter Delevoryas <peter.delevoryas@colorado.edu>
2017-04-23 15:37:43 -0600

    Removed test url

--- eat-net/src/main/java/net/eat/controllers/IndexController.java
+++ eat-net/src/main/java/net/eat/controllers/IndexController.java
@@ -14,12 +14,4 @@ public class IndexController {
 	public String index(Model model) {
 		return "index";
 	}
-
-    @GetMapping("/{name}/{password}")
-    public String account(@PathVariable String name,
-                          @PathVariable String password,
-                          Model model) {
-        PersistentStore.insert(name, password);
-        return "index";
-    }
 }
