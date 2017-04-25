46a37e29e38755ab855c57bd6625dd65de813025
humu7371 <hulaif.muhammadilyas@colorado.edu>
2017-04-25 17:30:46 -0600

    reset

--- eat-net/src/main/java/net/eat/controllers/IndexController.java
+++ eat-net/src/main/java/net/eat/controllers/IndexController.java
@@ -3,15 +3,24 @@ package net.eat.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.PathVariable;
 
 import net.eat.restaurant.RestaurantStore;
-import net.eat.PersistentStore;
 
 @Controller
 public class IndexController {
 	@GetMapping("/")
-	public String index(Model model) {
-		return "index2";
+	public String index(@CookieValue(value="username", defaultValue="") String username,
+                        @CookieValue(value="password", defaultValue="") String password,
+                        Model model) {
+        model.addAttribute("username", username);
+        model.addAttribute("password", password);
+        return "index2";
 	}
+
+    @GetMapping("/light")
+    public String index(Model model) {
+		return "index2-light";
+    }
 }
