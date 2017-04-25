780e7943fbc84ed5c7a37cfa82a1d37fac2336e4
humu7371 <hulaif.muhammadilyas@colorado.edu>
2017-04-25 16:56:30 -0600

    reset

--- eat-net/src/main/java/net/eat/controllers/CommentController.java
+++ eat-net/src/main/java/net/eat/controllers/CommentController.java
@@ -2,6 +2,8 @@ package net.eat.controllers;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import net.eat.restaurant.comment;
+
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -10,22 +12,60 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 /**
  * Servlet implementation class CommentController
  */
 @Controller
-public class CommentController extends HttpServlet {
+public class CommentController {
+//extends HttpServlet {
+	
+//	private String name;
+//	private String email;
+//	private String comment;
+//	private String restaurantName;
+//	
+//	public CommentController (String name, String email, String restaurantName, String comment) {
+//		this.name = name;
+//		this.email = email;
+//		this.restaurantName = restaurantName;
+//		this.comment = comment;
+//	}
+	
 	private static final long serialVersionUID = 1L;
        
     /**
      * @see HttpServlet#HttpServlet()
      */
     public CommentController() {
-        super();
+        //super();
         // TODO Auto-generated constructor stub
+    }
+
+    //@RequestMapping(value = "/comment", method = RequestMethod.POST)
+    //public ArrayList() {
+    	
+    //}
+    
+    @PostMapping("/comment")
+    public String ArrayList(@RequestParam("name") String name,
+    						@RequestParam("email") String email,
+    						@RequestParam("restaurantName") String restaurantName,
+    						@RequestParam("comment") String paragraph,
+    						Model model) {
+    	comment comment = new comment(name, restaurantName, email, paragraph);
+    	return "Hello";
+    }
+    
+    @GetMapping("/comment")
+    public String GetArrayList(Model model) {
+    	return "comment";
     }
 
 	@RequestMapping(value = "/post_comment", method = RequestMethod.POST)
@@ -37,7 +77,8 @@ public class CommentController extends HttpServlet {
 		RequestDispatcher rd = request.getRequestDispatcher("login_validation?account=mike");
 		rd.forward(request,  response);
 		//return "welcomeMessage";
-	}	
+	}
+	
 	
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
