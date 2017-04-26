13089e2257a3c302b372a6495b4d3e6da2a5dbf6
Mac <user@engr2-25-7-dhcp.int.colorado.edu>
2017-04-25 19:54:24 -0600

    comment update

--- eat-net/src/main/java/net/eat/controllers/IndexController.java
+++ eat-net/src/main/java/net/eat/controllers/IndexController.java
@@ -3,24 +3,30 @@ package net.eat.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.CookieValue;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import net.eat.restaurant.RestaurantStore;
 
 @Controller
 public class IndexController {
-	@GetMapping("/")
-	public String index(@CookieValue(value="username", defaultValue="") String username,
-                        @CookieValue(value="password", defaultValue="") String password,
+	@PostMapping("/")
+	public String index(@RequestParam(value="dname", defaultValue="") String username,
+                        @RequestParam(value="url", defaultValue="") String durl,
+                        @RequestParam(value="comment", defaultValue="") String comment,
                         Model model) {
         model.addAttribute("username", username);
         model.addAttribute("password", password);
+        model.addAttribute("shouldDisplayComment", true);
+        model.addAttribute("comment", comment);
         return "index2";
 	}
 
-    @GetMapping("/light")
-    public String index(Model model) {
-		return "index2-light";
+    @GetMapping("/")
+    public String displayNoComment(Model model) {
+    	model.addAttribute("shouldDisplayComment", false);
+    	
+		return "/";
     }
 }
