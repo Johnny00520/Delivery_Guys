72f7cc11de605ddca1876572bd8c04084fe332c2
humu7371 <hulaif.muhammadilyas@colorado.edu>
2017-04-28 16:32:21 -0600

    update from master

--- eat-net/src/main/java/net/eat/controllers/OrderController.java
+++ eat-net/src/main/java/net/eat/controllers/OrderController.java
@@ -1,15 +1,14 @@
 package net.eat.controllers;
 
 import java.io.IOException;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class OrderController{
-	@PostMapping("/order")
+	@GetMapping("/order")
 	public String Order(Model model) {
-		return "orderview"
+		return "orderview";
 	}
 }
---
+++ eat-net/src/main/java/net/eat/restaurant/RestaurantOrder.java
@@ -0,0 +1,10 @@
+package net.eat.restaurant;
+
+public class RestaurantOrder {
+	private int OrderID;
+	
+	private int GetOrderID() {
+		return this.OrderID;
+	}
+
+}
