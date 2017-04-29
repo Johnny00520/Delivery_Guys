d7bf4e079e9e70bfb7c18b3af820bda617a4729c
Peter Delevoryas <peter.delevoryas@colorado.edu>
2017-04-28 19:16:53 -0600

    Modified restaurant controller to display restaurant name and items

--- eat-net/spring-boot/src/main/java/net/eat/web/RestaurantController.java
+++ eat-net/spring-boot/src/main/java/net/eat/web/RestaurantController.java
@@ -1,8 +1,11 @@
 package net.eat.web;
 
+import java.util.ArrayList;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,22 +13,30 @@ import org.springframework.ui.Model;
 
 import net.eat.domain.RestaurantDao;
 import net.eat.domain.Restaurant;
+import net.eat.domain.Item;
+import net.eat.domain.ItemDao;
 
 @Controller
 public class RestaurantController {
     @Autowired
-    private RestaurantDao restaurantDao;
+    private RestaurantDao restaurants;
 
-    @PostMapping("/restaurant")
+    @Autowired
+    private ItemDao items;
+
+    @PostMapping("/restaurant/create")
     public String createRestaurant(@RequestParam(value="name") String name) {
         Restaurant restaurant = new Restaurant(name);
-        this.restaurantDao.save(restaurant);
+        this.restaurants.save(restaurant);
         return "index";
     }
 
-    @GetMapping("/restaurant")
-    public String restaurantForm(Model model) {
-        model.addAttribute("test", "testing");
+    @GetMapping("/restaurant/{name}")
+    public String restaurantForm(@PathVariable(value="name") String name, Model model) {
+        Restaurant restaurant = this.restaurants.findByName(name);
+        ArrayList<Item> items = this.items.findAllByRestaurant(restaurant);
+        model.addAttribute("restaurant", restaurant);
+        model.addAttribute("items", items);
         return "restaurant";
     }
 }
