035aa16384d86d14e3cc90cb75331da1801a2276
Peter Delevoryas <peter.delevoryas@colorado.edu>
2017-04-29 03:54:36 -0600

    Added ssl and simple security config
    I added the configuration secrets in application.properties
    and the keystore because the grader might not appreciate
    having to generate their own keystore and writing their
    own application-prod.yml, and because we're just going to
    run this locally anyways.

---
+++ eat-net/spring-boot/src/main/java/net/eat/SecurityConfig.java
@@ -0,0 +1,23 @@
+package net.eat;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.core.userdetails.User;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Bean
+    public UserDetailsService userDetailsService() {
+        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
+        manager.createUser(User.withUsername("user").password("password").roles("USER").build());
+        return manager;
+    }
+}
